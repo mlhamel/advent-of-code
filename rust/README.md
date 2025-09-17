70bc0a3de8a8b98acac4025578abdded4f303d68
Mathieu Leduc-Hamel <marrakis@gmail.com>
2025-09-16 23:20:58 -0400

    Re-organize repos structure.

--- rust/README.md
+++ rust/README.md
@@ -7,5 +7,7 @@ This directory contains my solutions for the Advent of Code challenges implement
 You can run the Rust solutions using `cargo`. Each day's solution is organized in a separate subdirectory.
 
 ```bash
-$ cargo run 1.0 inputs/01/content.txt
+$ cargo run 2023 1.0 inputs/2023/01/content.txt
 ```
+
+Replace `2023` with the year, `1.0` with the day and part (e.g., `1.0` for Day 1 Part 1, `1.1` for Day 1 Part 2), and provide the path to the input file.
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,15 @@
 use std::env;
 use std::fs;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
+pub mod year2023;
+pub mod year2024;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let day: &String = &args[1]; // day
-    let input: &String = &args[2]; // input
-    let parameters: &Option<&String> = &args.get(3); // parameters
+    let year: &String = &args[1]; // year
+    let day: &String = &args[2]; // day
+    let input: &String = &args[3]; // input
+    let parameters: &Option<&String> = &args.get(4); // parameters
 
     let contents = fs::read_to_string(input)
         .expect("Should have been able to read the file");
@@ -19,21 +18,21 @@ fn main() {
         .unwrap_or(&String::from(""))
         .to_string();
 
+    println!("Year: {:?}", year);
     println!("Day: {:?}", day);
     println!("Input: {:?}", input);
     println!("Contents: {:?}", contents);
 
-    let day_num = day.as_str();
-
-    let value = match day_num {
-        "1.0" => day1::first_1_0(contents),
-        "1.1" => day1::first_1_1(contents),
-        "2.0" => day2::second_1_0(contents, parsed_parameters),
-        "2.1" => day2::second_1_1(contents),
-        "3.0" => day3::third_1_0(contents),
-        "3.1" => day3::third_1_1(contents),
-        "4.0" => day4::four_1_0(contents),
-        "4.1" => day4::four_1_1(contents),
+    let value = match (year.as_str(), day.as_str()) {
+        ("2023", "1.0") => year2023::day1::first_1_0(contents),
+        ("2023", "1.1") => year2023::day1::first_1_1(contents),
+        ("2023", "2.0") => year2023::day2::second_1_0(contents, parsed_parameters),
+        ("2023", "2.1") => year2023::day2::second_1_1(contents),
+        ("2023", "3.0") => year2023::day3::third_1_0(contents),
+        ("2023", "3.1") => year2023::day3::third_1_1(contents),
+        ("2023", "4.0") => year2023::day4::four_1_0(contents),
+        ("2023", "4.1") => year2023::day4::four_1_1(contents),
+        // Add 2024 matches here as needed
         _ => Err(-1)
     };
 
